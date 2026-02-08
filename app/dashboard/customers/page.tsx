68deaa093db82c1d6f20369e3f298d056e7fdc37
adriansv15 <santosh9@illinois.edu>
2026-02-07 18:50:04 -0600

    added customers table to customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,36 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+// import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+// import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Customers',
+  title: 'Invoices',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const formattedCustomersTable = await fetchFilteredCustomers(query)
+//   const currentPage = Number(searchParams?.page) || 1;
+//   const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}> */}
+      <Suspense key={query}>
+        {/* <Table query={query} currentPage={currentPage} /> */}
+        <Table customers={formattedCustomersTable} />
+      </Suspense>
+    </div>
+  );
 }
